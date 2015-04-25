c84de0aa2d4ef12ced192f18befc3a58f6989281
pm-roy-selig <roy.selig@pubmatic.com>
2015-04-25 15:51:44 -0400

    move inline script out of file

---
+++ tablesort-config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by learningcubed on 4/24/15.
+ */
+TableSort.arrowNone = ' <img height=10 width=10 src="blank.gif" alt="">';
+TableSort.arrowUp   = ' <img height=10 width=10 src="arrow-up.gif" alt="&uarr;">';
+TableSort.arrowDown = ' <img height=10 width=10 src="arrow-down.gif" alt="&darr;">';
